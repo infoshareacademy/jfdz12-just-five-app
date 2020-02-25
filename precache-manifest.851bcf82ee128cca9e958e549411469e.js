eb64ede3905f4f42e73a37d015477dde2f816ba2
Szymon Wiergowski <s.wiergowski@gmail.com>
2020-02-25 21:54:39 +0100

    Updates

---
+++ precache-manifest.851bcf82ee128cca9e958e549411469e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2a7baa9be422c9f9bcf029a5933f0a4",
+    "url": "/drink-app/index.html"
+  },
+  {
+    "revision": "3aa147d504402f8daf2d",
+    "url": "/drink-app/static/css/main.c991d6f0.chunk.css"
+  },
+  {
+    "revision": "c9c635544379413c90a9",
+    "url": "/drink-app/static/js/2.d68bb5d8.chunk.js"
+  },
+  {
+    "revision": "29cd909fd605c5e2d99ff1367c319d8f",
+    "url": "/drink-app/static/js/2.d68bb5d8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3aa147d504402f8daf2d",
+    "url": "/drink-app/static/js/main.19cf6c65.chunk.js"
+  },
+  {
+    "revision": "ad875c4f6c0d4d1226cd",
+    "url": "/drink-app/static/js/runtime-main.4e36133f.js"
+  },
+  {
+    "revision": "434fa62203a17e4e3079f2e2992309a0",
+    "url": "/drink-app/static/media/alkomat_safety.434fa622.png"
+  },
+  {
+    "revision": "a6ccc8f1cc7c38e48e07f53f42e4e215",
+    "url": "/drink-app/static/media/drink.a6ccc8f1.png"
+  },
+  {
+    "revision": "5bad8da81bc92c87fd011ebe2bf16a4b",
+    "url": "/drink-app/static/media/image_dashboard.5bad8da8.png"
+  },
+  {
+    "revision": "08070e8424101e6392db68452a5aea67",
+    "url": "/drink-app/static/media/image_dashboard_small.08070e84.png"
+  },
+  {
+    "revision": "2aea4b73e5fd353f1bf27ea36359613e",
+    "url": "/drink-app/static/media/map_shop.2aea4b73.png"
+  }
+]);
